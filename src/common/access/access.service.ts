0f5cc7bfed4d1b770985c1454fbde28f1acb12fc
EstebanC <aprendizti@estudiodemoda.com.co>
2026-09-22 09:38:40 -0500

    modulo que permite validar el acceso a los endpoints

---
+++ src/common/access/access.service.ts
@@ -0,0 +1,20 @@
+import { HttpException, Injectable, HttpStatus } from "@nestjs/common";
+import type { Request } from "express";
+import { UsuariosParkingService } from "../../routes/usuariosParking/usuariosParking.service.js";
+import { GraphRestService } from "../graph/graphRest.service.js";
+import { ConfigService } from "@nestjs/config"
+
+@Injectable()
+export class AccessService{
+
+    constructor(private readonly usuariosParkingService: UsuariosParkingService){
+    }
+
+    async hasAccess(req : Request, graphToken:string, allowedRoles: Array<"Usuario" | "Admin">): Promise<boolean>{
+        let rol : "Usuario" | "Admin" = await this.usuariosParkingService.getRole(graphToken, req)
+        if(allowedRoles.includes(rol)){
+            return true
+        }
+        return false
+    }
+}
